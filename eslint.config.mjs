86063cb5f62705aefd857906dc865f8756e78892
Munsat <munsat_111@hotmail.com>
2025-01-16 16:09:09 +1100

    disabling ts rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-interface": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
